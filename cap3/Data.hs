305bb9bab8f6f38b64d2b844485d9dc9dd45af6c
luis-antonio <luis.developer.ac@gmail.com>
2021-09-20 20:26:17 -0500

    desestuturação a partir de tipos

--- cap3/Data.hs
+++ cap3/Data.hs
@@ -1,8 +1,21 @@
 -- Dia: sum type
 -- Segunda, terça, quarta...: values constructors
+import Data.Text.Internal.Unsafe.Char (ord)
 data Dia = Segunda | Terca | Quarta | Quinta | Sexta | Sabado | Domingo
 
 agenda :: Dia -> String
 agenda Domingo = "TV..."
 agenda Sabado = "Festa"
 agenda _ = "Trabalho"
+
+
+data Pessoa = Fisica String Int | Juridica String
+
+verPessoa :: Pessoa -> (String, String)
+verPessoa (Fisica nome idade) = ("Nome: " ++ nome, "Idade:" ++ show idade)
+verPessoa (Juridica razao_social) = ("Juridica: " ++ razao_social, "Não há idade")
+
+
+newtype UUID = UUID String
+uuidVersion :: UUID -> Int
+uuidVersion (UUID x) = ord (head x)
