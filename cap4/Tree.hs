3fb3493a9d7374ba18dc029255efd6a9eb8bfc6b
Luis Antonio <luis.developer.ac@gmail.com>
2021-10-06 23:10:47 -0500

    link para a arvore de exemplo

--- cap4/Tree.hs
+++ cap4/Tree.hs
@@ -1,5 +1,6 @@
 data Tree a = Null | Leaf a | Node (Tree a) a (Tree a) deriving Show
 
+-- Árvore usada como exemplo: https://bityli.com/lEuC3m
 tree :: Tree Int
 leftTree = Node (Node (Null) 9 (Node (Leaf 12) 14 (Null))) 17 (Node (Leaf 19) 23 (Null))
 rightTree = Node (Node (Null) 54 (Node (Leaf 67) 72 (Null))) 76 (Null)
